9db93968a0a233d58183d892c572af34b0167e96
ColbyLe <colbyle97@gmail.com>
2019-04-22 23:29:48 -0700

    - moved images into resources directory - added fonts

---
+++ src/Homepage.java
@@ -0,0 +1,82 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class Homepage {
+    VBox homeBox;
+
+    Homepage() {
+        homeBox = getHomepage();
+    }
+
+    public VBox getContent() {
+        return homeBox;
+    };
+
+    private static VBox getHomepage() {
+        Image pmImg = new Image("file:resources/images/MP_overlook.jpg");
+        ImageView pm = new ImageView(pmImg);
+
+        Label newsLabel = new Label("What's New at PMPL");
+        newsLabel.setPrefWidth(976);
+        newsLabel.setAlignment(Pos.BASELINE_LEFT);
+
+        Label[] newsInfo = new Label[4];
+        for(int i=0; i<4; i++) {
+            newsInfo[i] = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
+                    "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
+                    "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+                    "sunt in culpa qui officia deserunt mollit anim id est laborum.");
+            newsInfo[i].setPrefWidth(976);
+            newsInfo[i].setWrapText(true);
+
+        }
+
+        Label[] newsTitles = new Label[4];
+        for(int i=0; i<4; i++) {
+            newsTitles[i] = new Label("Lorem ipsum dolor sit amet");
+            newsTitles[i].setPrefWidth(976);
+            newsTitles[i].setAlignment(Pos.BASELINE_LEFT);
+        }
+
+        VBox newsBox = new VBox(newsLabel, newsTitles[0], newsInfo[0], newsTitles[1], newsInfo[1], newsTitles[2], newsInfo[2], newsTitles[3], newsInfo[3]);
+        newsBox.setPadding(new Insets(32,24,32,24));
+        newsBox.setSpacing(8);
+
+        Label info = new Label("PMPL HOURS OF OPERATION:\n" +
+                "Monday 10:00 am - 6:00 pm \n" +
+                "Tuesday 10:00 am - 5:00 pm \n" +
+                "Wednesday 12:00 pm - 8:00 pm \n" +
+                "Thursday 10:00 am - 5:00 pm \n" +
+                "Friday 12:00 pm - 4:00 pm \n" +
+                "Saturday 12:00 pm - 4:00 pm \n" +
+                "\n" +
+                "\n" +
+                "Pocono Mountain Public Library \n" +
+                "5500 Municipal Drive \n" +
+                "Coolbaugh Township Municipal Center \n" +
+                "Tobyhanna, PA 18466 \n" +
+                "\n" +
+                "\n" +
+                "Phone: (570) 894-8860 \n" +
+                "Fax: (570) 894-8852");
+        info.setPrefWidth(976);
+        VBox justInfo = new VBox(info);
+        justInfo.setAlignment(Pos.CENTER);
+        justInfo.setPadding(new Insets(24,24,32,24));
+        justInfo.setSpacing(8);
+
+        VBox homepage = new VBox(pm, newsBox, justInfo);
+        return homepage;
+    }
+}
--- src/PoconoMountainPL.java
+++ src/PoconoMountainPL.java
@@ -1,6 +1,9 @@
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.Scene;
@@ -17,19 +20,23 @@ public class PoconoMountainPL extends Application {
     }
 
     public void start(Stage primaryStage) {
-        VBox homeBox = new VBox(getTopNav(), getHomepage(), getBottomNav());
+        Homepage home = new Homepage();
+
+        VBox homeBox = new VBox(getTopNav(), home.getContent(), getBottomNav());
 
         ScrollPane homePane = new ScrollPane(homeBox);
         homePane.setPrefViewportHeight(768);
 
         primaryScene = new Scene(homePane);
+        primaryScene.getStylesheets().add("main.css");
         primaryStage.setScene(primaryScene);
         primaryStage.show();
     }
 
-    private VBox getTopNav() {
+    private static VBox getTopNav() {
         Image bannerImg = new Image("file:resources/images/banner.png");
         ImageView banner = new ImageView(bannerImg);
+        /*
         Button[] loginB = new Button[2];
         loginB[0] = new Button("Log In");
         loginB[1] = new Button("Sign Up");
@@ -44,8 +51,8 @@ public class PoconoMountainPL extends Application {
         buttonBar.setPadding(new Insets(32,32,0,32));
         buttonBar.setPrefWidth(384);
         buttonBar.setAlignment(Pos.BASELINE_CENTER);
-
-        HBox bannerBar = new HBox(banner, buttonBar);
+        */
+        HBox bannerBar = new HBox(banner);
 
         Button[] navB = new Button[4];
         navB[0] = new Button("Home");
@@ -53,6 +60,16 @@ public class PoconoMountainPL extends Application {
         navB[2] = new Button("Events");
         navB[3] = new Button("About");
         TextField searchBar = new TextField("Find books and more");
+        for(Button x:navB) {
+            x.setOnMouseEntered(e -> {
+                x.setStyle("-fx-background-color: rgb(0,131,131)");
+            });
+
+            x.setOnMouseExited(e -> {
+                x.setStyle("-fx-background-color:  rgb(0,121,121)");
+            });
+        }
+
 
         for(Button x:navB){
             x.setPrefWidth(160);
@@ -69,7 +86,7 @@ public class PoconoMountainPL extends Application {
         return topNav;
     }
 
-    private HBox getBottomNav() {
+    private static HBox getBottomNav() {
         Hyperlink[] navLink = new Hyperlink[3];
         navLink[0] = new Hyperlink("Contact Us");
         navLink[1] = new Hyperlink("Site Map");
@@ -81,65 +98,4 @@ public class PoconoMountainPL extends Application {
         botNav.setAlignment(Pos.CENTER_RIGHT);
         return botNav;
     }
-
-    private VBox getHomepage() {
-        Image pmImg = new Image("file:resources/images/MP_overlook.jpg");
-        ImageView pm = new ImageView(pmImg);
-
-        Label newsLabel = new Label("What's New at PMPL");
-        newsLabel.setPrefWidth(976);
-        newsLabel.setAlignment(Pos.BASELINE_LEFT);
-
-        Label[] newsInfo = new Label[4];
-        for(int i=0; i<4; i++) {
-            newsInfo[i] = new Label("Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip " +
-                    "ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
-                    "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
-                    "sunt in culpa qui officia deserunt mollit anim id est laborum.");
-            newsInfo[i].setPrefWidth(976);
-            newsInfo[i].setWrapText(true);
-
-        }
-
-        Label[] newsTitles = new Label[4];
-        for(int i=0; i<4; i++) {
-            newsTitles[i] = new Label("Lorem ipsum dolor sit amet");
-            newsTitles[i].setPrefWidth(976);
-            newsTitles[i].setAlignment(Pos.BASELINE_LEFT);
-        }
-
-        VBox newsBox = new VBox(newsLabel, newsTitles[0], newsInfo[0], newsTitles[1], newsInfo[1], newsTitles[2], newsInfo[2], newsTitles[3], newsInfo[3]);
-        newsBox.setPadding(new Insets(32,24,32,24));
-        newsBox.setSpacing(8);
-
-        Label info = new Label("PMPL HOURS OF OPERATION:\n" +
-                "Monday 10:00 am - 6:00 pm \n" +
-                "Tuesday 10:00 am - 5:00 pm \n" +
-                "Wednesday 12:00 pm - 8:00 pm \n" +
-                "Thursday 10:00 am - 5:00 pm \n" +
-                "Friday 12:00 pm - 4:00 pm \n" +
-                "Saturday 12:00 pm - 4:00 pm \n" +
-                "\n" +
-                "\n" +
-                "Pocono Mountain Public Library \n" +
-                "5500 Municipal Drive \n" +
-                "Coolbaugh Township Municipal Center \n" +
-                "Tobyhanna, PA 18466 \n" +
-                "\n" +
-                "\n" +
-                "Phone: (570) 894-8860 \n" +
-                "Fax: (570) 894-8852");
-        info.setPrefWidth(976);
-        VBox justInfo = new VBox(info);
-        justInfo.setAlignment(Pos.CENTER);
-        justInfo.setPadding(new Insets(24,24,32,24));
-        justInfo.setSpacing(8);
-
-        VBox homepage = new VBox(pm, newsBox, justInfo);
-        return homepage;
-    }
-
-
 }
